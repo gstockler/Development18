1aed647ffbcfe8ee9644466450aee6b7dce8d4aa
gstockler <43679782+gstockler@users.noreply.github.com>
2019-01-25 11:23:52 +0100

    Add files via upload

---
+++ Ug_PS1_GGSB.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 24 19:03:16 2019
+
+@author: gabi
+"""
+
+# =============================================================================
+#               UGANDA 2013-2014 SURVEY DATA ANALYSIS
+#
+#                           DEVELOPMENT PS1
+# =============================================================================
+
+"""
+
+                Q1 - CIW Analysis & Inequality
+
+"""
+
+#%% 1.1) 
+"""
+Report average CIW per household separately for rural and urban areas.
+
+"""
+
+### General
+
+
+## Urban vs Rural areas
+
+#%% 1.2) 
+"""
+
+- Show histogram for CIW separately for rural and urban areas; 
+- Report the variance of logs for CIW separately for rural and urban areas.
+
+"""
+
+
+#%% 1.3)
+
+"""
+
+Describe the joint cross-sectional behavior of CIW
+
+"""
+
+#%% 1.4)
+
+"""
+4. Describe the CIW level, inequality, and covariances over the lifecycle.
+
+"""
+
+#%% 1.4)
+
+"""
+Rank your households by income, and dicuss the behavior of the top and bottom of the
+consumption and wealth distributions conditional on income.
+
+"""
+
